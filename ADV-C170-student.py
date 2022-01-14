d7050367e6a79cab7620a1b0ba4347de114ee78e
Anantjit <anantjitchander@gmail.com>
2022-01-14 15:41:21 +0530

    Add files via upload

---
+++ ADV-C170-student.py
@@ -0,0 +1,54 @@
+"""
+Created on Fri Jan 14 15:22:54 2022
+
+@author: Anant
+"""
+
+
+class History_in_space():
+     def year_2008(self):
+         print("\n", "Events related to space which happened in 2008", "\n")
+         print("India successfully launched Chandrayaan in 2008 in order to explore the moon.The craft was inserted into lunar orbit in its first attempt. One of the greatest achievement of Chandrayaan was the discovery of the water molecules in the lunar soil.")
+
+     def year_2011(self):
+         print("\n", "Events related to space which happened in 2011", "\n")
+
+
+class History_in_medicine():
+     def year_2008(self):
+         print("\n", "Events related to Medical Science which happened in 2008", "\n")
+         print("Although scientists have long discovered how malaria is transferred and know how to prevent it, nearly 1 million people die every year from the disease, according to international estimates.Finally, Dec. 8, the first results of a malaria vaccine that shows promise hit international news")
+
+     def year_2011(self):
+                   print(
+                       "\n", "Events related to Medical Science which happened in 2008", "\n")
+
+
+class History_in_sports():
+     def year_2008(self):
+         print("\n", "Events related to Sports which happened in 2011", "\n")
+         print("In 2008, Abhinav Bindra created history by bagging gold in men's 10m air rifle event at the Beijing Games. His gold remains the country's first and only individual gold medal till date.")
+
+         def year_2011(self):
+             print("\n", "Events related to Sports which happened in 2011", "\n")
+
+
+
+
+
+
+
+
+space = History_in_space()
+space.year_2008()
+space.year_2011()
+ 
+ 
+medical = History_in_medicine()
+medical.year_2008()
+medical.year_2011()
+ 
+ 
+sports = History_in_sports()
+sports.year_2008()
+
